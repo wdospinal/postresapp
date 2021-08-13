058976a9a7cbd3b272d41e5ff5a4e30474c7345f
Restyled.io <commits@restyled.io>
2021-08-13 22:49:40 +0000

    Restyled by prettier

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,16 @@
-const {getDefaultConfig} = require("metro-config");
+const { getDefaultConfig } = require("metro-config");
 
 module.exports = (async () => {
-  const {resolver : {sourceExts, assetExts}} = await getDefaultConfig();
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
   return {
-    transformer : {
-      babelTransformerPath : require.resolve("react-native-svg-transformer")
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
     },
-    resolver : {
-      assetExts : assetExts.filter(ext => ext !== "svg"),
-      sourceExts : [...sourceExts, "svg" ]
-    }
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"],
+    },
   };
 })();
