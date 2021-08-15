a77ba5a11adcfb2ab5365b39ec534c6fc0b6fc75
Restyled.io <commits@restyled.io>
2021-08-15 22:25:34 +0000

    Restyled by prettier

--- e2e/tests/welcome.e2e.js
+++ e2e/tests/welcome.e2e.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-undef */
-describe('Postres', () => {
+describe("Postres", () => {
   beforeAll(async () => {
     await device.launchApp({
-      permissions : {
-        location : 'inuse',
+      permissions: {
+        location: "inuse",
       },
     });
   });
 
-  beforeEach(async () => { await device.reloadReactNative(); });
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
 
-  it('should have main screen', async () => {
-    await waitFor(element(by.id('createDessert')))
-        .toBeVisible()
-        .withTimeout(5000);
+  it("should have main screen", async () => {
+    await waitFor(element(by.id("createDessert")))
+      .toBeVisible()
+      .withTimeout(5000);
   });
 });
